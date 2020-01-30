845a7135fdd39de6b3f2e16174a0fe8f78705c35
kobra5639 <45314036+kobra5639@users.noreply.github.com>
2020-01-30 12:00:10 +0100

    Add files via upload

---
+++ modeNormal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structD.h"
+
+
+int statut(int x){
+
+
+}
+
+void inserer(int x, int y){
+
+
+}
+
+void JouerNormal1vs1(int mat[N][M]){
+
+  int x=0;
+
+  do{
+
+    printf("Choisissez ou vous aller mettre votre piece (numero de colonne entre 0 et 6): ");
+    scanf("%d\n",&x);
+
+  }while (x<0||x>6);
+
+  inserer(x,statut(x));
+
+  verifer
+
+}
---
+++ structD.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N  6
+#define N  7
+
+
+
+char mat[N][M];
+
+
+typedef struct joueurs_s {
+
+  char * nom;
+  char * couleur;
+
+
+}joueur ;
+
+
+typedef enum piece_s {bloquante,creuse,pleine} piece;
