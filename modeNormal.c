459210ef7083f3bb0e7c7300616ec7ae1541404b
Haxxor72 <46872331+Haxxor72@users.noreply.github.com>
2020-02-03 15:12:49 +0100

    Add files via upload

--- modeNormal.c
+++ modeNormal.c
@@ -5,14 +5,9 @@
 
 int statut(int x, char mat[N][M]){
 
-  for (int y = M ; (mat[x][y]==("R"||"J")) || (y < 0) ; y--) ; /* on arrive au bout tant que l'emplacement est pris on remonte */
+  for (int y = M ; (mat[x][y]==("R"||"J")) || (y > 0) ; y--) ; /* on arrive au bout tant que l'emplacement est pris on remonte */
 
-  if (y=>0) {          /* si les coordonnée sur l'axe des y est dans la grille alors on le retourne */
     return y ;
-  }
-  else{               /* sinon on retourne -1 */
-    return -1;
-  }
 
 }
 
@@ -27,7 +22,71 @@ void inserer(int x, int y, joueur t, char mat[N][M]){
 
 }
 
-int quiGagne(char mat[N][M],joueur t){       // fonction qui verifie les pions du joueur pour savoir si il a gagner ou pas
+int quiGagne(char mat[N][M],joueur j){       // fonction qui verifie les pions du joueur pour savoir si il a gagner ou pas
+  int i,j=0;
+  int cptL = 0;
+  int cptD = 0;
+  int cptC = 0;
+
+
+  if(j.couleur = 'R'){
+      for( i = 0; i < N || cptL<4; i++) {
+
+        for(j = 0; j < M || cptL<4; j++) {
+
+          if(mat[i][j] == 'R'){
+
+            cptL ++;
+          }
+
+          if (mat[i][j] == 'J') {
+            cptL = 0;
+          }
+        }
+        cptL = 0;
+      }
+
+      for( i = 0; i < M || cptC<4; i++) {
+
+        for(j = 0; j < N || cptC<4; j++) {
+
+          if(mat[i][j] == 'R'){
+
+            cptC ++;
+          }
+
+          if (mat[i][j] == 'J') {
+            cptC = 0;
+          }
+        }
+        cptC = 0;
+      }
+
+
+      for( i = N; i > 0 || cptD<4; i++) {
+
+        for(j = M; j > 0 || cptD<4; j++) {
+
+          if (mat[i][j] == 'R') {
+
+          }
+
+        }
+
+      }
+
+
+
+
+
+
+
+
+  }
+
+
+
+
 
 }
 
@@ -47,13 +106,13 @@ while( quiGagne(mat,j1)!=1 || quiGagne(mat,j2)!=1 ){        // tant qu'il n'y a 
     printf("Joueur 1 :Choisissez ou vous aller mettre votre piece (numero de colonne entre 0 et 6):\n");
     scanf("%d",&x);
 
-  if (statut(x)==-1) {
+  if (statut(x,mat)==-1) {
     printf("Erreur sur les coordonnée des y : la colonne %d est rempli essayer une autre \n",&x);
   }
 
-  }while ((x<0||x>6) || statut(x)==-1);
+}while ((x<0||x>6) || statut(x,mat)==-1);
 
-  inserer(x,statut(x),j1,mat);                          //on insere la piece
+  inserer(x,statut(x,mat),j1,mat);                          //on insere la piece
 
   if(quiGagne(mat,j1)==0){                              // on passe au tour suivant si et selement si le joueur 1 n'a pas gagner
 
@@ -65,13 +124,13 @@ while( quiGagne(mat,j1)!=1 || quiGagne(mat,j2)!=1 ){        // tant qu'il n'y a 
       printf("Joueur2 : Choisissez ou vous aller mettre votre piece (numero de colonne entre 0 et 6):\n");
       scanf("%d",&x);
 
-    if (statut(x)==-1) {
+    if (statut(x,mat)==-1) {
       printf("Erreur sur les coordonnée des y : la colonne %d est rempli essayer une autre \n",&x);
     }
 
-    }while ((x<0||x>6) || statut(x)==-1);
+  }while ((x<0||x>6) || statut(x,mat)==-1);
 
-    inserer(x,statut(x),j2,mat);
+    inserer(x,statut(x,mat),j2,mat);
   }
 
 }
