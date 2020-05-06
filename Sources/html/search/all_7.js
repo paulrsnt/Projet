98216048a2a1c2f597c6fe249a9ec8eb2abd3736
Aaron Amani <45314036+kobra5639@users.noreply.github.com>
2020-05-07 00:36:07 +0200

    Delete all_7.js

--- Sources/html/search/all_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['parcours_5fcolonne2v2',['parcours_colonne2v2',['../2v2_8c.html#a5aab50383ea263e2a0e942765c1654a7',1,'2v2.c']]],
-  ['parcours_5fdiagonale2v2',['parcours_diagonale2v2',['../2v2_8c.html#aa2916edc725d170266e8ae33dfb8ddc1',1,'2v2.c']]],
-  ['parcours_5fligne2v2',['parcours_ligne2v2',['../2v2_8c.html#ace0662ebfaf8aca90d8d21136bbda009',1,'2v2.c']]]
-];
