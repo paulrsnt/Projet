da2f4301d648caba9f8969a1c0819ddc9c9b8e08
Aaron Amani <45314036+kobra5639@users.noreply.github.com>
2020-05-11 16:43:38 +0200

    Delete functions_2.js

--- Sources/html/search/functions_2.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['initmatrice',['initMatrice',['../1v1_8c.html#a8ea0fc30fb52497aa2d549e539774e0e',1,'1v1.c']]],
-  ['initmatrice2v2',['initMatrice2v2',['../2v2_8c.html#ad43756677d82f8aa06b2bd7c67ed19ed',1,'2v2.c']]],
-  ['inserer',['inserer',['../1v1_8c.html#ab3a7b36c03544dd0cf050bab5a00dbe0',1,'1v1.c']]],
-  ['inserer2v2',['inserer2v2',['../2v2_8c.html#a7a59e25ed9aaca8c75861af3d1b67a2c',1,'2v2.c']]],
-  ['insereria',['insererIA',['../1vIA_8c.html#aa3c48bf593bfda674a52952e0ca89347',1,'1vIA.c']]],
-  ['inserermode1vsia',['insererMode1vsIA',['../1vIA_8c.html#a69adc8f155b90c9a3c7baffd670b56ec',1,'1vIA.c']]]
-];
